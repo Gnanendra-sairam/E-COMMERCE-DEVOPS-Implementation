3bb6d356eba2d652586fb6bfafafbbcff05c2848
Gnanendra-sairam <sairam.gutlapalli@gmail.com>
2026-03-06 22:25:21 +0530

    Add files via upload

---
+++ terraform/module/vpc/main.tf
@@ -0,0 +1,64 @@
+resource "aws_vpc" "e-commerce" {
+  cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support = true
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+  vpc_id = aws_vpc.e-commerce.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+  availability_zone = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id = aws_vpc.e-commerce.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+  availability_zone = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.e-commerce.id
+
+  
+}
+
+resource "aws_route_table" "r1" {
+  vpc_id = aws_vpc.e-commerce.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  
+}
+
+resource "aws_route_table_association" "r1_public" {
+  count = length(var.public_subnet_cidrs)
+  subnet_id = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.r1.id
+  
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id = aws_subnet.public[0].id
+  
+}
+
+resource "aws_route_table" "r2" {
+  vpc_id = aws_vpc.e-commerce.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+}
+}
+
+resource "aws_route_table_association" "r2_private" {
+  count = length(var.private_subnet_cidrs)
+  subnet_id = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.r2.id
+  
+}
