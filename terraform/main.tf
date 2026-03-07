59dabc40dc312c1450d7ad1ba7e159508449bf86
Gnanendra-sairam <sairam.gutlapalli@gmail.com>
2026-03-07 10:58:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./modules/vpc"
+  source = "./module/vpc"
 
   vpc_cidr             = var.vpc_cidr
   availability_zones   = var.availability_zones
@@ -30,11 +30,12 @@ module "vpc" {
 }
 
 module "eks" {
-  source = "./modules/eks"
+  source = "./module/eks"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnet_ids
   node_groups     = var.node_groups
+
 }
